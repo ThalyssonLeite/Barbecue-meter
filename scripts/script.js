ac2ece861d983c06cb19932f8a4d340d4cc75b35
Thalysson Leite <thalyssonl17@gmail.com>
2021-05-31 19:19:02 -0300

    preparing the HTML part. 1

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Carne - 500 gramas por pessoa + de 6 horaas - 650 gramas
+// Cerveja - 200 ml por pessoa + 6 horas - 350ml
+// Refrigerante/água - 700ml por pessoa + 6 horas - 1200ml
+
+// crianças valem 0.5
