670a58b606352e921da825a52e5c3f295224e22f
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-03 19:16:30 -0300

    I am finally happy, I did my best with the knowledge I have now. With my current knowledge with HTML, CSS and JavaScript that pretty little barbecuemeter is finally polished and finished (IT'S ON GOLD STAGE GUYS, YOU CAN MAKE THE CD'S OF CYBERPUN... cof cof BARBECUEMETER)

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,22 @@
-let emptyString = "";
 let adults;
-let childrens;
+let children;
 let duration;
 
 let inputAdults = document.getElementById("adult");
 let inputChildren = document.getElementById("children");
 let inputDuration = document.getElementById("duration");
 
-let meatResult = document.getElementById("meatResult");
+let resultTitle = document.getElementById("resultTitle");
+let meatField = document.getElementById("meatField");
+let alcoholicField = document.getElementById("alcoholicField");
+let drinksField = document.getElementById("drinksField");
 
 let result = document.getElementById("result");
 
-let totalAlcoholicBeverage;
-let totalDrinks;
-
 let meatPerPersonInGrams = 0.500;
-let standardBarbecueTime = 5;
+let alcholicBeveragePerPersonInMilliliters = 0.350;
+let normalDrinksAndWaterInMillimiters = 0.400;
+let standardBarbecueTimeInHours = 4;
 
 
 
@@ -23,58 +24,153 @@ let standardBarbecueTime = 5;
 function complementAdultsField() {
     adults = Number(inputAdults.value);
 
-    setTimeout(complement, 1000)
+    setTimeout(complement, 100);
 
     function complement() {
-        if (inputAdults.value.includes("adults", "Adults") == 0) {
-            inputAdults.value += " Adults"
+        switch (adults) {
+            case 1:
+                inputAdults.value += " adult";
+                break;
+
+            default:
+                inputAdults.value += " adults";
         }
     }
 }
 
 function complementChildrenField() {
-    childrens = Number(inputChildren.value);
+    if (inputChildren !== undefined) {
+        children = Number(inputChildren.value) / 2;
+    } else {
+        children = Number(0)
+    }
 
-    setTimeout(complement, 1000)
+    setTimeout(complement, 100)
 
     function complement() {
-        if (inputChildren.value.includes("Children") == 0) {
-            inputChildren.value += " Children"
+        switch (children) {
+            case 0.5:
+                inputChildren.value += " child";
+                break;
+
+            default:
+                inputChildren.value += " children";
         }
     }
 }
 
 function complementDurationField() {
-    i
+    duration = Number(inputDuration.value);
+    
+    setTimeout(complement, 100)
+
+    function complement() {
+        switch(duration) {
+            case 1:
+                inputDuration.value += " hour";
+                break;
+            
+            default: 
+                inputDuration.value += " hours";
+        }
+    }
 }
 
 function calculate() {
-    let duration = inputDuration.value;
    
-    if (duration < 1) {
+    if (duration === undefined || duration < 1) {
         duration = 1;
     }
 
-    if (duration > standardBarbecueTime) {
+    if (duration >= standardBarbecueTimeInHours) {
         meatPerPersonInGrams = 0.650;
+        alcholicBeveragePerPersonInMilliliters = 0.700;
+        normalDrinksAndWaterInMillimiters = 600;
+
     } else {
         meatPerPersonInGrams = 0.500;
+        alcholicBeveragePerPersonInMilliliters = 0.350;
+        normalDrinksAndWaterInMillimiters = 0.400;
     }
 
 
-    let totalMeat = calculateMeat();
 
-    function calculateMeat() {
-        return (adults + childrens) * meatPerPersonInGrams;
-    }
+    
+    calculateMeat();
+    calculateAlcoholicBeverage();
+    calculateDrinks();
 
-    meatResult.innerHTML = `${totalMeat.toFixed(1)}Kg de carne`;
+    checkValues();
+    
+
+    // adults = 0;
+    // children = 0;
+    // duration = 0;
 }
 
-function reset(field) {
-    if ((field.value.includes("Adults") || field.value.includes("Children")) || field.value.includes("Hours")) {
+function checkValues() {
+
+    if (adults == undefined && children == undefined ) {
+        adults = 0;
+        children = 0;
+    } else if (adults === undefined) {
+        adults = 0;
+    } else if (children === undefined) {
+        children = 0;
+    }
+
+    if (isNaN(children, adults)) {
+        resultTitle.innerHTML = "Ooops!"
+        meatField.innerHTML = "Please pay atention, fill out the fields using only numbers";
+
+        alcoholicField.style = "display: none";
+        drinksField.style = "display: none";
+
+        appearResult();
+    } else {
+        resultTitle.innerHTML = "You will need of:"
+
+        alcoholicField.style = "display: list-item";
+        drinksField.style = "display: list-item";
+
+        appearResult();
+    }
+}
+
+function calculateMeat() {
+    let totalMeat = (adults + children) * meatPerPersonInGrams;
+    meatField.innerHTML = `${totalMeat.toFixed(1)}Kg of meat`;
+}
+
+function calculateAlcoholicBeverage() {
+    let totalAlcoholicBeverage = adults * alcholicBeveragePerPersonInMilliliters;
+    alcoholicField.innerHTML = `${totalAlcoholicBeverage.toFixed(1)}L of alcoholic beverage`;
+}
+
+function calculateDrinks() {
+    let totalDrinks = children + adults * normalDrinksAndWaterInMillimiters;
+    drinksField.innerHTML = `${totalDrinks.toFixed(1)}L of water and soda` 
+}
+
+function appearResult() {
+    let h3 = document.getElementsByClassName("appearWhenPressCalculate")[0];
+    let resultDiv = document.getElementsByClassName("appearWhenPressCalculate")[1];
+
+    h3.style = "display: block";
+    resultDiv.style = "display: block";
+}
+
+function resetAdults(field) {
         field.value = "";
-    } 
+        adults = 0;
 }
 
+function resetChildren(field) {
+    field.value = "";
+    children = 0;
+}
 
+function resetDuration(field) {
+    field.value = "";
+    duration = 1;
+}
