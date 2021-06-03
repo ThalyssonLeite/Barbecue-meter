0c3409ec2a45508e57d02c8d95365a69db99fa68
Thalysson Leite <thalyssonl17@gmail.com>
2021-06-03 02:37:46 -0300

    I was kidding a little bit but I had an incredible idea, I will sleep now and start angain tomorow

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,52 @@
-let button = document.getElementById("button");
+let inputAdults = document.getElementById("adult");
+let inputChildren = document.getElementById("children");
+let inputDuration = document.getElementById("duration");
 
-button.addEventListener("click", userAlert)
-function userAlert() {
-    alert("olá")
+let meatResult = document.getElementById("meatResult");
+
+let result = document.getElementById("result");
+
+let totalMeat; 
+let totalAlcoholicBeverage;
+let totalDrinks;
+
+let meatPerPersonInGrams = 0.500;
+let standardBarbecueTime = 5;
+
+function calculate() {
+    let adults = Number(inputAdults.value);
+    let childrens = Number(inputChildren.value) / 2;
+    let duration = inputDuration.value;
+   
+    if (duration < 1) {
+        duration = 1;
+    }
+
+    if (duration > standardBarbecueTime) {
+        meatPerPersonInGrams = 0.650;
+    } else {
+        meatPerPersonInGrams = 0.500;
+    }
+
+
+    let totalMeat = calculateMeat();
+
+    function calculateMeat() {
+        return (adults + childrens) * meatPerPersonInGrams;
+    }
+
+    meatResult.innerHTML = `${totalMeat}Kg de carne`;
+}
+
+function complementAdultsField() {
+    let adults = inputAdults.value;
+    if (!(inputAdults.value.includes("adult"))) {
+        setTimeout(complete, 2500)
+    }
+
+    function complete() {
+        inputAdults.value += " adult"
+    }
+    
 }
 
